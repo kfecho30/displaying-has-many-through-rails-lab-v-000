7c7eb36af6a2bd9f0f8b0c6bd193e6e82eb4f88b
Kelsey Fecho <kfecho30@gmail.com>
2018-05-02 20:24:03 +0000

    automatically backed up by learn

--- db/migrate/20180502202328_create_appointments.rb
+++ db/migrate/20180502202328_create_appointments.rb
@@ -1,7 +1,8 @@
 class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
-
+      t.belongs_to :doctor
+      t.belongs_to :patient 
       t.timestamps null: false
     end
   end
